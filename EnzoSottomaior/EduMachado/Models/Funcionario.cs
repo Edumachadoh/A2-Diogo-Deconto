f6b4bc671b93cd4aef0040703dbb102d08a86003
Auto Logon <autologon@CSED.LAB>
2024-10-16 11:55:04 -0300

    Final commit

--- EnzoSottomaior/EduMachado/Models/Funcionario.cs
+++ EnzoSottomaior/EduMachado/Models/Funcionario.cs
@@ -7,5 +7,6 @@ public class Funcionario
     public string? Nome { get; set; }
     public string? Cpf { get; set; }
 
-    public ICollection<Folha>? Folhas { get; set; }
+    //public ICollection<Folha>? Folhas { get; set; }
+
 }
--- EnzoSottomaior/EduMachado/obj/Debug/net8.0/EduMachado.AssemblyInfo.cs
+++ EnzoSottomaior/EduMachado/obj/Debug/net8.0/EduMachado.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("EduMachado")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1ea914ad86b09b963cdfcd8f6891a14e9a549ae0")]
 [assembly: System.Reflection.AssemblyProductAttribute("EduMachado")]
 [assembly: System.Reflection.AssemblyTitleAttribute("EduMachado")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
